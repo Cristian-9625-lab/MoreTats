343752f9aabf11e95101ef12ec6f2812f9c36ca7
Cristian Trujillo <cristiantrujillo326@gmail.com>
2025-05-25 22:02:02 -0500

    moretats
    sitio promocional para artista de tatuajes

---
+++ moretats_website/js/main.js
@@ -0,0 +1,22 @@
+
+// Initialize AOS for scroll animations
+AOS.init();
+
+// Initialize Swiper slider
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  spaceBetween: 20,
+  slidesPerView: 1,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
